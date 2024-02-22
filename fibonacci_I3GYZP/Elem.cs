412a52320bf06e73c93b744102a584ecd50b00d6
polini46corvinus <patrik.simon@stud.uni-corvinus.hu>
2024-02-22 10:43:44 +0100

    #legkomolyabb fibonacci sorok

---
+++ fibonacci_I3GYZP/Elem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci_I3GYZP
+{
+    internal class Elem
+    {
+        public int Sorszám { get; set; }
+        public long Érték { get; set; }
+    }
+}
--- fibonacci_I3GYZP/Form1.cs
+++ fibonacci_I3GYZP/Form1.cs
@@ -5,13 +5,24 @@ namespace fibonacci_I3GYZP
         public Form1()
         {
             InitializeComponent();
+
+            List<Elem> lista = new List<Elem>();
+
             for (int i = 0; i < 10; i++)
             {
                 Button b = new Button();
                 b.Top = i * 30;
                 b.Text = Fibonacci(i).ToString();
                 Controls.Add(b);
+
+                Elem ujSor = new Elem();
+                ujSor.Sorszám = i;
+                ujSor.Érték = Fibonacci(i);
+
+                lista.Add(ujSor);
+
             }
+            dataGridView1.DataSource = lista;
         }
         int Fibonacci(int n)
         {
