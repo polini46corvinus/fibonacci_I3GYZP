ae6524205217693c4d585f5e332adbbd16d8283c
polini46corvinus <patrik.simon@stud.uni-corvinus.hu>
2024-03-14 10:47:18 +0100

    naonjo

---
+++ Hajós teszt/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Hajós_teszt
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            valaszGomb1 = new ValaszGomb();
+            valaszGomb2 = new ValaszGomb();
+            valaszGomb3 = new ValaszGomb();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(68, 287);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(625, 150);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(68, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // valaszGomb1
+            // 
+            valaszGomb1.BackColor = Color.LightGray;
+            valaszGomb1.BorderStyle = BorderStyle.None;
+            valaszGomb1.Location = new Point(68, 119);
+            valaszGomb1.Multiline = true;
+            valaszGomb1.Name = "valaszGomb1";
+            valaszGomb1.ReadOnly = true;
+            valaszGomb1.Size = new Size(423, 37);
+            valaszGomb1.TabIndex = 3;
+            // 
+            // valaszGomb2
+            // 
+            valaszGomb2.BackColor = Color.LightGray;
+            valaszGomb2.BorderStyle = BorderStyle.None;
+            valaszGomb2.Location = new Point(68, 175);
+            valaszGomb2.Multiline = true;
+            valaszGomb2.Name = "valaszGomb2";
+            valaszGomb2.ReadOnly = true;
+            valaszGomb2.Size = new Size(423, 37);
+            valaszGomb2.TabIndex = 4;
+            // 
+            // valaszGomb3
+            // 
+            valaszGomb3.BackColor = Color.LightGray;
+            valaszGomb3.BorderStyle = BorderStyle.None;
+            valaszGomb3.Location = new Point(68, 231);
+            valaszGomb3.Multiline = true;
+            valaszGomb3.Name = "valaszGomb3";
+            valaszGomb3.ReadOnly = true;
+            valaszGomb3.Size = new Size(423, 37);
+            valaszGomb3.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(559, 67);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(189, 187);
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(valaszGomb3);
+            Controls.Add(valaszGomb2);
+            Controls.Add(valaszGomb1);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private ValaszGomb valaszGomb1;
+        private ValaszGomb valaszGomb2;
+        private ValaszGomb valaszGomb3;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Hajós teszt/Form1.cs
@@ -0,0 +1,80 @@
+namespace Hajós_teszt
+{
+    public partial class Form1 : Form
+    {
+        List<Kerdes> OsszesKerdesek;
+        List<Kerdes> AktivKerdesek;
+
+        int AktivKerdes = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show(KerdesBeolvasas().Count.ToString());
+        }
+        private List<Kerdes> KerdesBeolvasas()
+        {
+            List<Kerdes> kerdesek = new List<Kerdes>();
+            StreamReader sr = new StreamReader("hajozasi_szabalyzat_kerdessor_BOM.txt");
+
+            while (!sr.EndOfStream)
+            {
+                string sor = sr.ReadLine() ?? string.Empty; //beolvasunk egy sort. HA NEM, akkor visszaadunk egy string.empty-t
+                string[] tomb = sor.Split("\t");
+                if (tomb.Length != 7) continue;
+
+                Kerdes k = new Kerdes()
+                {
+                    KerdesSzoveg = tomb[1],
+                    Valasz1 = tomb[2],
+                    Valasz2 = tomb[3],
+                    Valasz3 = tomb[4],
+                    URL = tomb[5]
+                };
+
+                int.TryParse(tomb[6], out int jovalasz);
+                k.HelyesValasz = jovalasz;
+
+                kerdesek.Add(k);
+
+
+            }
+            return kerdesek;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AktivKerdesek = new List<Kerdes>();
+            OsszesKerdesek = KerdesBeolvasas();
+
+            for (int i = 0; i < 7; i++)
+            {
+                AktivKerdesek.Add(OsszesKerdesek[i]);
+                OsszesKerdesek.RemoveAt(0);
+            }
+
+            dataGridView1.DataSource = AktivKerdesek;
+
+            KerdesMegjelenites(AktivKerdesek[3]);
+        }
+
+        void KerdesMegjelenites(Kerdes kerdes)
+        {
+            label1.Text = kerdes.KerdesSzoveg;
+            valaszGomb1.Text = kerdes.Valasz1;
+            valaszGomb2.Text = kerdes.Valasz2;
+            valaszGomb3.Text = kerdes.Valasz3;
+
+            if (!string.IsNullOrEmpty(kerdes.URL))
+            {
+                pictureBox1.Load("https://storage.altinum.hu/hajo/" + kerdes.URL);
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                pictureBox1.Visible = false;
+            }
+        }
+    }
+}
